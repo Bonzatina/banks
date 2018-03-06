cd9a00faeb61f412d7f0ecbeab6d3c9fa6a4f60e
Unknown <bonez@mail.ru>
2018-03-06 15:08:53 +0100

    for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+// server for heroku
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
